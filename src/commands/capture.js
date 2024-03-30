1c76ec4a4b841c360a2e87a2b1e77e3688e9f2ec
Ivan Kristianto <ivan@ivankristianto.com>
2024-03-30 12:16:03 +0700

    Refactor Babel configuration to use @babel/preset-env and @babel/plugin-transform-runtime

--- src/commands/capture.js
+++ src/commands/capture.js
@@ -16,17 +16,16 @@ exports.handler = async function (argv) {
 	// Set viewport width and height
 	await page.setViewport({ width: 1280, height: 720 });
 
-	await page.setCookie({
+	/* await page.setCookie({
 		name: 'wordpress_logged_in_cee5cf3b320767853e63e4035d47b9ad',
 		value: 'ivan.kristianto%40kaplan.com%7C1712925320%7CVItXw2UNVBYbbLy4xClkkqEV7aidLwbrbliOKHxX9m6%7C58966e42bb01ee5150c4593c99443a6be6722081493f197a4f75c60ee4fab84f',
 		domain: 'liverpool.kaplan-dev.altis.cloud',
 		path: '/',
-	});
+	}); */
 
 	await page.goto(url, { waitUntil: 'load' });
 
 	// Scroll to bottom of the page
-
 	await page.evaluate(() => {
 		window.scrollTo(0, document.body.scrollHeight);
 	});
